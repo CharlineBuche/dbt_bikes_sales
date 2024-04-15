c920ff0aa3051ffb88b16aa0819851fb0b04c64f
CharlineBuche <charline.buche@gmail.com>
2024-04-15 12:03:26 +0200

    modification sales_clean and top_products

--- models/intermediate/top_products.sql
+++ models/intermediate/top_products.sql
@@ -1,10 +1,21 @@
+WITH sub_category_top_products AS(
+
 SELECT
-
-Sub_Category,
-Product_Category,
-SUM(Revenue) as sumrev,
-ROW_NUMBER() OVER(ORDER BY SUM(Revenue) DESC) as rank_revenue
-
+    Product,
+    Product_Category,
+    SUM(Revenue) as sum_revenue,
+    /*ROW_NUMBER() OVER(ORDER BY SUM(Revenue) DESC) as rank_revenue,*/
+    SUM(Quantity) as sum_qty,
 FROM `keen-alignment-402208.dbt_bikes_sales.sales_clean`
 GROUP BY 1, 2
-/*QUALIFY rank_revenue <= 5*/
+/*QUALIFY rank_revenue <= 5*/)
+
+SELECT
+    Product,
+    Product_Category,
+    sum_qty,
+    sum_revenue,
+    ROUND(SAFE_DIVIDE(sum_revenue, sum_qty),2) as revenue_quantity,
+    ROW_NUMBER() OVER(ORDER BY SAFE_DIVIDE(sum_revenue, sum_qty) DESC) as rank_revenue_product,
+    ROW_NUMBER() OVER(ORDER BY sum_revenue DESC) as rank_revenue,
+FROM sub_category_top_products
