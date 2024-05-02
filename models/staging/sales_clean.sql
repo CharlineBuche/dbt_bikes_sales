c6fab520bb84252bc8177e08988496844f89e277
CharlineBuche <charline.buche@gmail.com>
2024-05-02 15:01:06 +0200

    changes

--- models/staging/sales_clean.sql
+++ models/staging/sales_clean.sql
@@ -1,19 +1,17 @@
 SELECT
-    Date as Date_sale,
-    CAST(Year AS INT) as Year_sale,
-    Month as Month_sale,
-    /*PARSE_DATE('%B %Y', CONCAT(Month, ' ', CAST(Year AS STRING))) as month_year_date,*/
-    FORMAT_DATE('%Y-%m', Date) AS year_month_date,
-    CAST(Customer_Age AS INT) as Customer_Age,
-    Customer_Gender,
-    Country,
-    State as State_sale,
-    Product_Category,
-    Sub_Category,
-    CAST(Quantity AS INT) as Quantity,
-    Unit_Cost,
-    ROUND(Unit_Price, 2) as Unit_Price,
-    Cost,
-    Revenue
+    Date AS date_sale,
+    CAST(Year AS INT) AS year_sale,
+    Month as month_sale,
+    FORMAT_DATE('%Y-%m', Date) AS date,
+    CAST(Customer_Age AS INT) AS customer_Age,
+    Customer_Gender AS customer_gender,
+    Country AS country,
+    Product_Category AS product_category,
+    Sub_Category AS subcategory,
+    CAST(Quantity AS INT) as quantity,
+    Unit_Cost AS unit_cost,
+    ROUND(Unit_Price, 2) as unit_price,
+    Cost AS cost,
+    Revenue AS revenue
 from {{ source('src_sales', 'sales') }}
 WHERE Date IS NOT NULL
